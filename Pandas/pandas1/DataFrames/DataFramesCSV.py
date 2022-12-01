363a1673647973a554c9a78fbfd77138a14b55e4
RobertoPatino1 <rapatino@espol.edu.ec>
2022-12-01 00:56:55 -0500

    Renamed file

--- Pandas/pandas1/DataFrames/DataFramesCSV.py
+++ Pandas/pandas1/DataFrames/DataFramesCSV.py
@@ -1,7 +1,7 @@
 import pandas as pd
-print("------Cargando archivos a DataFrames------")
+print("------Cargando archivos CSV a DataFrames------")
 #Podemos cargar datasets contenidos en archivos a un dataframe
-print("------1. Csv Files------")
+print("------1. Carga------")
 #Los archivos csv presentan una forma optima de almacenar datasets
 
 #Recuerda: Las rutas relativas inician en la carpeta raiz del proyecto
@@ -11,3 +11,10 @@ print(dataframeCsv1)
 #OJO: Para mostrar el dataframe completo se usa la funcion to_string()
 print("---Con to_string()---")
 print(dataframeCsv1.to_string())
+
+
+print("------2. Cambiar el total maximo de filas a leer------")
+#Esto sirve para evitar usar la funcion to_string()
+pd.options.display.max_rows = 9999
+print(pd.options.display.max_rows)
+print(dataframeCsv1)
